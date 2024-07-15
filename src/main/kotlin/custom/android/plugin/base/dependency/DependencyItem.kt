28bedaddd334db363f6082d9f2fe3e56d970ca64
wangkang <1226426603@qq.com>
2024-07-15 11:43:26 +0800

    增加情感云依赖

--- src/main/kotlin/custom/android/plugin/base/dependency/DependencyItem.kt
+++ src/main/kotlin/custom/android/plugin/base/dependency/DependencyItem.kt
@@ -54,11 +54,13 @@ object DependencyItem {
         "cn.entertech.android:affective-offline-sdk-flowtime:1.2.3"
 
     const val affectiveSdkApi = "cn.entertech.android:affective_sdk_api:1.2.3"
+    const val affectiveSdkCloud =
+        "cn.entertech.android:affective-cloud-sdk:1.2.3"
 
 
     const val entertechBase = "cn.entertech.android:base:latest.release"
 
-    const val room_version = "2.5.0"
+    private const val room_version = "2.5.0"
     const val database_room_runtime = "androidx.room:room-runtime:$room_version"
     const val database_room_compiler = "androidx.room:room-compiler:$room_version"
     const val database_room_ktx = "androidx.room:room-ktx:$room_version"

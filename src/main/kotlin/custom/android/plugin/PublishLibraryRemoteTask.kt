fc15596f03667327cd5f137ba4921a530d25f2b3
wk <1226426603@qq.com>
2024-10-01 20:28:55 +0800

    推给远程仓库的组件，不能以debug结尾

--- src/main/kotlin/custom/android/plugin/PublishLibraryRemoteTask.kt
+++ src/main/kotlin/custom/android/plugin/PublishLibraryRemoteTask.kt
@@ -16,17 +16,11 @@ open class PublishLibraryRemoteTask : BasePublishTask() {
 
     override fun checkPublishInfo(publishInfo: PublishInfo): Boolean {
         val version = publishInfo.version
-        // 正则表达式解释：
-        // \d+ 表示一个或多个数字（即非负整数）
-        // (\.\d+)+ 表示`.`后跟一个或多个数字（即版本号的段），并且这样的段可以出现一次或多次
-        // ^ 表示字符串开始
-        // $ 表示字符串结束
-        val regex = Regex("^\\d+(\\.\\d+)+$")
-        val result = regex.matches(version)
-        if (!result) {
-            PluginLogUtil.printlnErrorInScreen("$publishInfo version not matches d.d.d")
+        if(version.endsWith("debug")){
+            PluginLogUtil.printlnErrorInScreen("$publishInfo version end with debug")
+            return false
         }
-        return result
+        return true
     }
 
     override fun getPublishingExtensionRepositoriesPath(publishing: PublishingExtension): String {
